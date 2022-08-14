1082fcb364128e62f589138010e198fa8b89d05b
Jennifer <goodwije@gmail.com>
2022-08-14 14:06:48 -0600

    About Section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import coverImage from '../assets/cover/cover-image.png';
 
 const About = () => {
   return (
     <section className='my-5'>
       <h1 id='about'>About Me</h1>
       <img
-        src='../../src/assets/cover/cover-image.jpg'
+        src={coverImage}
         className='my-2'
         style={{ width: '100%' }}
         alt='cover'
