d5b0c75a5114ff541db03b65d7b2135275389e89
Jennifer <goodwije@gmail.com>
2022-08-29 21:59:45 -0600

    Added to the About me section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,37 +1,34 @@
 import React from 'react';
-import coverImage from '../assets/cover/cover-image.png';
 
 const About = () => {
   return (
     <section className='my-5'>
       <h1 id='about'>About Me</h1>
       <div class='container'>
-        <img
-          src={require('../assets/cover/cover-image.png')}
-          alt='another brick in the wall'
-        />
-      </div>
-      <div class='text-on-image'>
-        <h3> Shmeeheart </h3>
-        <p>
-          ...Jennifer was born in a large family who reveled in nick-naming each
-          other. No one knows how "Jennifer" warped into "Shmee" as a nickname,
-          but it stuck throughout the years. Shmee can be spicy at times, but
-          mainly she is a Shmeeheart ❤️ <br></br>
-          Jennifer has a natural curiosity for life and a desire to do and know
-          everthing, which allows her to relish in many hobbies and explore
-          various career paths. Technology is an area that has always sparked
-          Jennifer's interest, but had not taken the steps to pursue education
-          and experience further...until now. No matter what career, Jennifer
-          believes that staying abreast of technological advances is imperative
-          to success in the modern world. Her inquisitive nature has opened up
-          the door to a new career in technology. The unknown is both exciting
-          and terrifying and Jennifer welcomes the challenge. <br></br>
-          Outside of work, you can find Jennifer rocking out at a concert, in
-          the audience of a Broadway Musical, fishing, reading, listening to
-          true crime podcasts, playing her guitar, bowling, gardening or
-          singing!
-        </p>
+        <div class='text'>
+          <h3> Shmeeheart </h3>
+          <p>
+            Jennifer was born in a large family who reveled in nick-naming each
+            other. No one knows how "Jennifer" warped into "Shmee" as a
+            nickname, but it stuck throughout the years. Shmee can be spicy at
+            times, but mainly she is a Shmeeheart ❤️
+            <br></br>
+            Jennifer has a natural curiosity for life and a desire to do and
+            know everthing, which allows her to relish in many hobbies and
+            explore various career paths. Technology is an area that has always
+            sparked Jennifer's interest, but had not taken the steps to pursue
+            education and experience further...until now. No matter what career,
+            Jennifer believes that staying abreast of technological advances is
+            imperative to success in the modern world. Her inquisitive nature
+            has opened up the door to a new career in technology. The unknown is
+            both exciting and terrifying and Jennifer welcomes the challenge.
+            <br></br>
+            Outside of work, you can find Jennifer rocking out at a concert, in
+            the audience of a Broadway Musical, fishing, reading, listening to
+            true crime podcasts, playing her guitar, bowling, gardening or
+            singing!
+          </p>
+        </div>
       </div>
     </section>
   );
